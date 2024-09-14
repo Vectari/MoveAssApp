5f663ee74924771eebb0796b135bfcc100f5ad4b
Vectari <mateusz.majer996@gmail.com>
2024-09-14 10:53:46 +0200

    feat: add dictionary

---
+++ src/library/dictionary.ts
@@ -0,0 +1,18 @@
+const dictionary = {
+  HomePage: {
+    pl: "",
+    en: "",
+  },
+  LogInPage: {
+    pl: "",
+    en: "",
+  },
+  SignUpPage: {
+    pl: "",
+    en: "",
+  },
+};
+
+export default dictionary;
+
+export type dictionaryType = typeof dictionary;
