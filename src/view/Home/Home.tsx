89fd3b28d86d3bc2c7942f084344b10ad6a68cca
Vectari <mateusz.majer996@gmail.com>
2024-09-14 11:27:48 +0200

    feat: test light/dark mode

--- src/view/Home/Home.tsx
+++ src/view/Home/Home.tsx
@@ -1,7 +1,37 @@
+import styled, { ThemeProvider } from "styled-components";
+import { theme } from "../../theme";
+import { useState } from "react";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.background};
+  border: 1px solid ${(props) => props.theme.border};
+  color: ${(props) => props.theme.primary};
+`;
+
+const Button = styled.button`
+  background-color: ${(props) => props.theme.primary};
+  border: none;
+  color: white;
+  padding: 10px;
+  cursor: pointer;
+  &:hover {
+    background-color: ${(props) => props.theme.hover.primary};
+  }
+`;
+
 export function Home() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
   return (
     <>
       <h1>Home page</h1>
+      <ThemeProvider theme={isDarkMode ? theme.darkMode : theme.lightMode}>
+        <Container>
+          <Button onClick={() => setIsDarkMode(!isDarkMode)}>
+            Toggle Mode
+          </Button>
+        </Container>
+      </ThemeProvider>
     </>
   );
 }
