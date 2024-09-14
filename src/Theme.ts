448cffe506599697bb1d619fe5e0ab0e6a6dcdd4
Vectari <mateusz.majer996@gmail.com>
2024-09-14 11:26:55 +0200

    feat: add colors to theme

--- src/Theme.ts
+++
@@ -1,6 +0,0 @@
-export const Theme = {
-  colors: {
-    white: "rgb(255,255,255)",
-    black: "rgb(0,0,0)",
-  },
-};
---
+++ src/theme.ts
@@ -0,0 +1,26 @@
+export const theme = {
+  lightMode: {
+    background: "#F5F5F5",
+    primary: "#4CAF50",
+    secondary: "#FF9800",
+    active: "#2196F3",
+    border: "#E0E0E0",
+    hover: {
+      primary: "#45A049", // Darker green for hover
+      secondary: "#FB8C00", // Darker orange for hover
+      active: "#1976D2", // Darker blue for active hover
+    },
+  },
+  darkMode: {
+    background: "#121212",
+    primary: "#4CAF50",
+    secondary: "#FFB74D",
+    active: "#90CAF9",
+    border: "#333333",
+    hover: {
+      primary: "#388E3C", // Darker green for hover
+      secondary: "#FF9800", // Bright orange for hover
+      active: "#64B5F6", // Lighter blue for active hover
+    },
+  },
+};
