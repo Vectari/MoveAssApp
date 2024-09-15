820b8edee804b1dc0673fd49844470dd9604761d
Vectari <mateusz.majer996@gmail.com>
2024-09-15 15:53:06 +0200

    feat: add navigate go home after login and add autologin button

--- src/view/LogIn/LogIn.tsx
+++ src/view/LogIn/LogIn.tsx
@@ -2,21 +2,31 @@ import { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../library/firebaseConfig";
 import dictionary from "../../library/dictionary";
+import { useNavigate } from "react-router-dom";
 
 export function LogIn() {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [error, setError] = useState<string | null>(null);
 
+  const navigate = useNavigate();
+
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
+
     try {
       await signInWithEmailAndPassword(auth, email, password);
       // Redirect or show a success message
       console.log("LogIn Success!");
+      navigate("/");
     } catch (error) {
       setError(error.message);
     }
+  };
+
+  const handleAutoLogin = () => {
+    setEmail("test@test.com");
+    setPassword("test123");
   };
 
   return (
@@ -46,6 +56,7 @@ export function LogIn() {
       </form>
       <p>test@test.com</p>
       <p>test123</p>
+      <button onClick={handleAutoLogin}>AutoLogin</button>
     </>
   );
 }
