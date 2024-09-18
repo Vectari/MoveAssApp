9a34aba5cd6e451cb0396fbdebe26178fa6a4671
Vectari <mateusz.majer996@gmail.com>
2024-09-19 00:20:26 +0200

    chore: delete language change code

--- src/view/LogIn/LogIn.tsx
+++ src/view/LogIn/LogIn.tsx
@@ -4,8 +4,6 @@ import { auth } from "../../library/firebaseConfig";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "../../hooks/useTranslation";
 
-import { useLanguage } from "../../hooks/useLanguage";
-
 export function LogIn() {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
@@ -13,8 +11,6 @@ export function LogIn() {
 
   const navigate = useNavigate();
   const { translate } = useTranslation();
-
-  const { language, setLanguage } = useLanguage();
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -61,17 +57,14 @@ export function LogIn() {
         </div>
         <button type="submit">{translate("LogInPage", "loginButton")}</button>
       </form>
-      <p>test@test.com</p>
-      <p>test123</p>
-      <button onClick={handleAutoLogin}>AutoLogin</button>
 
+      <br />
+      <br />
+      <br />
       <div>
-        <button onClick={() => setLanguage("en")} disabled={language === "en"}>
-          English
-        </button>
-        <button onClick={() => setLanguage("pl")} disabled={language === "pl"}>
-          Polish
-        </button>
+        <p>test@test.com</p>
+        <p>test123</p>
+        <button onClick={handleAutoLogin}>AutoLogin</button>
       </div>
     </>
   );
