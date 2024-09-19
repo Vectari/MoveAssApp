e47a5af36cb05d14a5fbc69496c386d8d9443515
Vectari <mateusz.majer996@gmail.com>
2024-09-20 01:35:50 +0200

    feat: add user panel after login

---
+++ src/view/UserPanel/UserPanel.tsx
@@ -0,0 +1,36 @@
+import { onAuthStateChanged, signOut, User } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { auth } from "../../library/firebaseConfig";
+import { useNavigate } from "react-router-dom";
+
+export function UserPanel() {
+  const navigate = useNavigate();
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+
+    if (auth.currentUser === null) {
+      console.log("Redirect to login");
+      navigate("/login");
+    }
+
+    return () => unsubscribe();
+  }, [navigate]);
+
+  const handleLogout = async () => {
+    await signOut(auth);
+    console.log(auth.currentUser);
+    navigate("/login");
+  };
+
+  return (
+    <>
+      <h1>User panel</h1>
+      <div>{user ? <p>Welcome, {user.email}</p> : ""}</div>
+      <button onClick={handleLogout}>Logout</button>
+    </>
+  );
+}
