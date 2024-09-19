81bcfea51a7f540806725c88aa39f4a386be01a1
Vectari <mateusz.majer996@gmail.com>
2024-09-19 20:36:19 +0200

    feat: add language select component

---
+++ src/components/LanguageSelect/LanguageSelect.style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledLanguageSelectWrapper = styled.span`
+  cursor: pointer;
+
+  img {
+    scale: 0.6;
+  }
+`;
---
+++ src/components/LanguageSelect/LanguageSelect.tsx
@@ -0,0 +1,35 @@
+import PL_FLG from "/pl_flag.svg";
+import UK_FLAG from "/uk_flag.svg";
+import { useLanguage } from "../../hooks/useLanguage";
+import { Dictionary } from "../../library/dictionary";
+import { StyledLanguageSelectWrapper } from "./LanguageSelect.style";
+
+export function LanguageSelect() {
+  const { language, setLanguage } = useLanguage();
+
+  setLanguage((localStorage.getItem("language") as Dictionary) || "en");
+
+  return (
+    <StyledLanguageSelectWrapper>
+      {language === "pl" ? (
+        <img
+          src={UK_FLAG}
+          onClick={() => {
+            setLanguage("en");
+            localStorage.setItem("language", "en");
+          }}
+          alt=""
+        />
+      ) : (
+        <img
+          src={PL_FLG}
+          onClick={() => {
+            setLanguage("pl");
+            localStorage.setItem("language", "pl");
+          }}
+          alt=""
+        />
+      )}
+    </StyledLanguageSelectWrapper>
+  );
+}
