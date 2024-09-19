7c5b17ecaf85e0781f619339c350849d00f9f366
Vectari <mateusz.majer996@gmail.com>
2024-09-19 20:36:04 +0200

    chore: move language select to component

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,36 +1,14 @@
 import { NavLink } from "react-router-dom";
 import { StyledNavBar } from "./NavBar.styled";
-import { useLanguage } from "../../hooks/useLanguage";
-import { Dictionary } from "../../library/dictionary";
+import { LanguageSelect } from "../LanguageSelect/LanguageSelect";
 
 export function NavBar() {
-  const { language, setLanguage } = useLanguage();
-
-  setLanguage((localStorage.getItem("language") as Dictionary) || "en");
-
   return (
     <StyledNavBar>
       <NavLink to="/">Home</NavLink>
       <NavLink to="/login">Log In</NavLink>
       <NavLink to="/signup">Sign Up</NavLink>
-      <button
-        onClick={() => {
-          setLanguage("en");
-          localStorage.setItem("language", "en");
-        }}
-        disabled={language === "en"}
-      >
-        English
-      </button>
-      <button
-        onClick={() => {
-          setLanguage("pl");
-          localStorage.setItem("language", "pl");
-        }}
-        disabled={language === "pl"}
-      >
-        Polish
-      </button>
+      <LanguageSelect />
     </StyledNavBar>
   );
 }
