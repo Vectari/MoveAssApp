c8a1edcc466e08581279c0210f0a98ba26a0d200
Vectari <mateusz.majer996@gmail.com>
2024-09-29 23:22:02 +0200

    style: basic portal styles

---
+++ src/components/Portal/Portal.styled.ts
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const PortalWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  div {
+    background: rgb(77, 77, 77);
+    padding: 2rem;
+    border-radius: 8px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    position: relative;
+  }
+
+  button {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    background: red;
+    color: white;
+    border: none;
+    padding: 0.5rem 1rem;
+    cursor: pointer;
+    border-radius: 4px;
+  }
+`;
