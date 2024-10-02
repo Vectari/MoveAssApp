3c83e3ffaf1e225e744d4c0cfa3d1139f86845ce
Vectari <mateusz.majer996@gmail.com>
2024-10-02 08:58:17 +0200

    feat: get data from firestore

--- src/components/ProgressChart/ProgressChart.tsx
+++ src/components/ProgressChart/ProgressChart.tsx
@@ -1,4 +1,36 @@
+import { collection, query, getDocs } from "firebase/firestore";
+import { auth, db } from "../../library/firebaseConfig";
+import { useEffect } from "react";
+
 export function ProgressChart() {
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const userId = user.uid;
+        const q = query(collection(db, "users", userId, "progress"));
+        try {
+          const querySnapshot = await getDocs(q);
+
+          querySnapshot.forEach((doc) => {
+            const docId = doc.id;
+
+            if (docId >= "2023-10-01") {
+              console.log(docId, " => ", doc.data());
+              console.log(doc.data());
+            }
+          });
+        } catch (error) {
+          console.error("Error fetching progress data: ", error);
+        }
+      } else {
+        console.log("ERROR!! No user logged in");
+      }
+    });
+
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  }, []);
+
   return (
     <>
       <h1>Progress Chart</h1>
