e4a731b36e132754c095e09c51e7475395cb886e
Vectari <mateusz.majer996@gmail.com>
2024-10-05 11:47:22 +0200

    feat: add useCallback

--- src/hooks/useTranslation.tsx
+++ src/hooks/useTranslation.tsx
@@ -1,12 +1,16 @@
+import { useCallback } from "react";
 import { useLanguage } from "../hooks/useLanguage";
 import dictionary, { Dictionary } from "../library/dictionary";
 
 export const useTranslation = () => {
   const { language } = useLanguage();
 
-  const translate = (page: Dictionary, element: string) => {
-    return dictionary[page]?.[element]?.[language] || "";
-  };
+  const translate = useCallback(
+    (page: Dictionary, element: string) => {
+      return dictionary[page]?.[element]?.[language] || "";
+    },
+    [language] // Add dictionary and language as dependencies
+  );
 
   return { translate };
 };
