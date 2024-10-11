940194cdb68a47cbae33fa729f1d0718a25757f1
Vectari <mateusz.majer996@gmail.com>
2024-10-12 00:04:07 +0200

    feat: add atoms

---
+++ src/atoms/atoms.ts
@@ -0,0 +1,7 @@
+import { atom } from "jotai";
+
+export const atomShowDailyKcal = atom<boolean>(false);
+export const atomShowDailyKcalStreak = atom<boolean>(false);
+export const atomShowWeightTarget = atom<boolean>(false);
+export const atomShowDimChart = atom<boolean>(false);
+export const atomShowWeightChart = atom<boolean>(false);
