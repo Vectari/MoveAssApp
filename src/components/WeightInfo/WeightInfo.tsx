6ebf6cc99803820b6d1a6bb215d644ba54265292
Vectari <mateusz.majer996@gmail.com>
2024-10-14 00:03:14 +0200

    feat: add WeightInfo component

---
+++ src/components/WeightInfo/WeightInfo.tsx
@@ -0,0 +1,10 @@
+export function WeightInfo() {
+  return (
+    <>
+      <h3>Weight Info:</h3>
+      <p>weight target</p>
+      <p>latest weight</p>
+      <p>weight to loose</p>
+    </>
+  );
+}
