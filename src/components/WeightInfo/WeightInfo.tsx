addda80718910e632c0bd194a5ca1ad91889d96e
Vectari <mateusz.majer996@gmail.com>
2024-10-15 00:41:10 +0200

    feat: add WeightInfo content

--- src/components/WeightInfo/WeightInfo.tsx
+++ src/components/WeightInfo/WeightInfo.tsx
@@ -1,16 +1,61 @@
 import { useAtom } from "jotai";
-import { atomLatestWeight, atomWeightTarget } from "../../atoms/atoms";
+import {
+  atomLatestWeight,
+  atomShowWeightInfo,
+  atomWeightTarget,
+} from "../../atoms/atoms";
+import { useEffect } from "react";
+import { doc, getDoc } from "firebase/firestore";
+import { auth, db } from "../../library/firebaseConfig";
+import { onAuthStateChanged } from "firebase/auth";
 
 export function WeightInfo() {
-  const [weightTarget] = useAtom(atomWeightTarget);
+  const [weightTarget, setWeightTarget] = useAtom(atomWeightTarget);
   const [latestWeight] = useAtom(atomLatestWeight);
+  // const [showWeightInfo] = useAtom(atomShowWeightInfo);
+
+  const [showWeightInfo, setShowWeightInfo] =
+    useAtom<boolean>(atomShowWeightInfo);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async () => {
+      if (auth.currentUser !== null) {
+        const userId = auth.currentUser.uid;
+
+        const userShowWeightInfo = await getDoc(
+          doc(db, "users", userId, "show_hide", "showWeightInfo")
+        );
+
+        const userWeightDoc = await getDoc(
+          doc(db, "users", userId, "weight_target", "weightTarget")
+        );
+
+        if (userShowWeightInfo.exists()) {
+          const showWeightInfoData = userShowWeightInfo.data();
+          setShowWeightInfo(showWeightInfoData?.showWeightInfo);
+          // setLoaded(true);
+        }
+
+        const weightData = userWeightDoc.data();
+        setWeightTarget(weightData?.weightTarget || "");
+      }
+    });
+
+    return () => unsubscribe();
+  });
 
   return (
     <>
-      <h3>Weight Info</h3>
-      <p>Weight Target: {weightTarget}</p>
-      <p>Latest Weight: {latestWeight}</p>
-      <p>Weight to lose: {latestWeight - weightTarget}</p>
+      {showWeightInfo && (
+        <>
+          <hr />
+          <h3>Weight Info</h3>
+          <p>Weight Target: {weightTarget}</p>
+          <p>Latest Weight: {latestWeight}</p>
+          <p>Weight to lose: {latestWeight - Number(weightTarget)}</p>
+          <hr />
+        </>
+      )}
     </>
   );
 }
