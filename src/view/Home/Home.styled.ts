ba5b1b6ada2c34f0a1e966c432317fe6f0124ce6
Vectari <mateusz.majer996@gmail.com>
2024-10-19 11:55:10 +0200

    feat: base home styles

---
+++ src/view/Home/Home.styled.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const HomeWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 2 1fr;
+`;
+
+export const Logo = styled.svg`
+  width: 256px;
+  height: 256px;
+  grid-column: 1;
+`;
+
+export const Header = styled.h1`
+  grid-column: 2;
+
+  p {
+    margin-left: 10rem;
+    font-size: 1.2rem;
+  }
+`;
+
+export const SubHeaderInfo = styled.article`
+  grid-column: 2;
+  max-width: 500px;
+  font-size: 1rem;
+  font-weight: 300;
+`;
