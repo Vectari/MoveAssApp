804a82135280165dab80b3d8b41965cb93bd11a3
Vectari <mateusz.majer996@gmail.com>
2024-10-25 00:06:18 +0200

    style: add style

--- src/components/NavBar/NavBar.styled.ts
+++ src/components/NavBar/NavBar.styled.ts
@@ -1,16 +1,26 @@
 import styled from "styled-components";
 
 export const StyledNavBar = styled.div`
-  padding: 1rem;
+  width: 640px;
 
   a {
     text-decoration: none;
     padding: 1rem;
     font-size: 1.5rem;
+    vertical-align: middle;
     color: ${(props) => props.theme.text.primary};
   }
 
   .active {
     color: ${(props) => props.theme.navbar.active};
   }
+`;
+
+export const Button = styled.button`
+  /* color: ${(props) => props.theme.text.primary}; */
+  background-color: ${(props) => props.theme.secondaryButton};
+  border: none;
+  border-radius: 10px;
+  padding: 10px;
+  margin: 5px;
 `;
