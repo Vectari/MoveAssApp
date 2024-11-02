471ec3069320230ad00a5d8f40bae010b20716bf
Vectari <mateusz.majer996@gmail.com>
2024-11-02 20:58:41 +0100

    feat: add checked to props

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,10 +1,21 @@
+import React from "react";
+
 interface InputProps {
   type: string;
   id: string;
-  value: string;
+  value?: string;
+  checked?: boolean;
   onChange: React.ReactEventHandler;
 }
 
-export function Input({ type, id, value, onChange }: InputProps) {
-  return <input type={type} id={id} value={value} onChange={onChange} />;
+export function Input({ type, id, value, checked, onChange }: InputProps) {
+  return (
+    <input
+      type={type}
+      id={id}
+      value={value}
+      checked={checked}
+      onChange={onChange}
+    />
+  );
 }
