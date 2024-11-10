145832fe726f91d968f0cf5121aacf407aa847dc
Vectari <mateusz.majer996@gmail.com>
2024-11-10 18:45:32 +0100

    style: change input style

--- src/components/Input/Input.styled.ts
+++ src/components/Input/Input.styled.ts
@@ -4,42 +4,30 @@ export const InputWrapper = styled.div`
   position: relative;
   margin-top: 1.5rem;
 
-  /* Chrome, Safari, Edge, Opera */
-  /* input::-webkit-outer-spin-button,
-  input::-webkit-inner-spin-button {
-    -webkit-appearance: none;
-    margin: 0;
-  } */
-
-  /* Firefox */
-  /* input[type="number"] {
-    appearance: textfield;
-  } */
-
   label {
-    position: absolute;
+    /* position: absolute;
     top: 0;
     left: 0;
     transform: translate(8px, -30%);
-    background-color: grey; /* Match input background or set as transparent */
+    background-color: grey;
     padding: 0 5px;
     font-size: 1rem;
     font-weight: bold;
-    color: white; /* Label color */
+    color: white; */
   }
 
   input {
-    width: 100%;
+    /* width: 100%;
     padding: 10px;
-    padding-top: 18px; /* Add space for the label */
+    padding-top: 18px;
     font-size: 1rem;
     border: 1px solid #ccc;
     border-radius: 4px;
     outline: none;
-    background-color: grey;
+    background-color: grey; */
   }
 
   input:focus {
-    border-color: #007bff; /* Border color on focus */
+    /* border-color: #007bff; */
   }
 `;
